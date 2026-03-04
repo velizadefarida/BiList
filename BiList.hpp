512449b5d05ad544b68c0596f1bd6e15beb33ada
Farida <velizadefarida@gmail.com>
2026-03-04 23:38:02 +0300

    added insert and add

--- BiList.hpp
+++ BiList.hpp
@@ -15,3 +15,25 @@ BiList<T>* createList() {
     fake->next = fake->prev = nullptr;
     return fake;
 }
+
+template<typename T>
+BiList<T>* add(BiList<T>* fake, const T& value) {
+    BiList<T>* newNode = new BiList<T>{value, nullptr, nullptr};
+    BiList<T>* last = fake;
+    while (last->next) last = last->next;
+    last->next = newNode;
+    newNode->prev = last;
+    return newNode;
+}
+
+template<typename T>
+BiList<T>* insert(BiList<T>* node, const T& value) {
+    if (!node) return nullptr;
+    BiList<T>* newNode = new BiList<T>{value, nullptr, nullptr};
+    BiList<T>* nextNode = node->next;
+    newNode->next = nextNode;
+    newNode->prev = node;
+    node->next = newNode;
+    if (nextNode) nextNode->prev = newNode;
+    return newNode;
+}
